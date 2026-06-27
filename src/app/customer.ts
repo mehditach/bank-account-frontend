d26700a7492ae8e1e604ba708e5c660d6900dfbc
mehditach <tachtouktelmehdi@gmail.com>
2026-06-27 15:29:20 +0100

    Gestion complete des clients - CRUD et recherche

--- src/app/customer.ts
+++ src/app/customer.ts
@@ -15,7 +15,7 @@ export class Customer {
 
   private apiUrl = 'http://localhost:8080/customers';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getCustomers(): Observable<CustomerModel[]> {
     return this.http.get<CustomerModel[]>(this.apiUrl);
@@ -27,5 +27,16 @@ export class Customer {
 
   addCustomer(customer: CustomerModel): Observable<CustomerModel> {
     return this.http.post<CustomerModel>(this.apiUrl, customer);
+  }
+  updateCustomer(id: number, customer: CustomerModel): Observable<CustomerModel> {
+    return this.http.put<CustomerModel>(this.apiUrl + '/' + id, customer);
+  }
+  deleteCustomer(id: number): Observable<any> {
+    return this.http.delete(this.apiUrl + '/' + id);
+  }
+  searchCustomers(keyword: string): Observable<CustomerModel[]> {
+    return this.http.get<CustomerModel[]>(this.apiUrl + '/search', {
+      params: { keyword }
+    });
   }
 }
--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -1,24 +1,73 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Customer, CustomerModel } from '../customer';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
 export class Customers {
   customers = signal<CustomerModel[]>([]);
+  searchKeyword: string = '';
+
+  newCustomer: CustomerModel = { name: '', email: '' };
+  editingCustomer: CustomerModel | null = null;
 
   constructor(private customerService: Customer) {
+    this.loadCustomers();
+  }
+
+  loadCustomers() {
     this.customerService.getCustomers().subscribe({
-      next: (data) => {
-        console.log('Données reçues:', data);
-        this.customers.set(data);
-      },
-      error: (err) => {
-        console.log('Erreur:', err);
+      next: (data) => this.customers.set(data)
+    });
+  }
+
+  search() {
+    if (this.searchKeyword.trim() === '') {
+      this.loadCustomers();
+      return;
+    }
+    this.customerService.searchCustomers(this.searchKeyword).subscribe({
+      next: (data) => this.customers.set(data)
+    });
+  }
+
+  addCustomer() {
+    if (this.newCustomer.name.trim() === '') return;
+    this.customerService.addCustomer(this.newCustomer).subscribe({
+      next: () => {
+        this.newCustomer = { name: '', email: '' };
+        this.loadCustomers();
       }
+    });
+  }
+
+  startEdit(customer: CustomerModel) {
+    this.editingCustomer = { ...customer };
+  }
+
+  saveEdit() {
+    if (this.editingCustomer && this.editingCustomer.id) {
+      this.customerService.updateCustomer(this.editingCustomer.id, this.editingCustomer).subscribe({
+        next: () => {
+          this.editingCustomer = null;
+          this.loadCustomers();
+        }
+      });
+    }
+  }
+
+  cancelEdit() {
+    this.editingCustomer = null;
+  }
+
+  deleteCustomer(id: number | undefined) {
+    if (!id) return;
+    this.customerService.deleteCustomer(id).subscribe({
+      next: () => this.loadCustomers()
     });
   }
 }
